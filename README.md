d0f670cfc48e12afb8e2de3d7a5857f05f052c87
Jose-Jorge HERNANDEZ <josejorgehz@gmail.com>
2026-04-15 01:47:21 -0600

    A lightweight Android WebView wrapper for 'RunningRiot', an HTML5-based game developed for learning purposes.

---
+++ README.md
@@ -0,0 +1,18 @@
+## Description
+RunningRiot is an experimental mobile experience that bridges web-based gaming with native Android functionality. This project serves as a dedicated Android Wrapper (WebView) for the HTML5 game RunningRiot, optimized for performance and mobile accessibility.
+
+## Key Features
+Full-screen WebView integration: Seamlessly renders HTML5 canvas.
+
+Splash Screen: Custom branded loading experience.
+
+Optimized Performance: JavaScript enabled with hardware acceleration support.
+
+Navigation Handling: Integrated Android back-button support for in-game navigation.
+
+## Tech Stack
+Android Studio (Java/Kotlin)
+
+Android WebView API
+
+HTML5 / JavaScript (Game Core)
---
+++ app/src/main/java/com/uvm/runningriot/MainActivity.java
@@ -0,0 +1,29 @@
+package com.uvm.runningriot;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+            public class MainActivity extends AppCompatActivity {
+
+                @Override
+                protected void onCreate(Bundle savedInstanceState) {
+                    super.onCreate(savedInstanceState);
+                    setContentView(R.layout.activity_main);
+
+                    WebView webView = findViewById(R.id.miWebView);
+
+                    WebSettings webSettings = webView.getSettings();
+                    webSettings.setJavaScriptEnabled(true);
+
+                    webView.setWebViewClient(new WebViewClient());
+
+                    webView.loadUrl("https://git.apx.ltd/testbed/seed032026/index.html");
+                }
+}
